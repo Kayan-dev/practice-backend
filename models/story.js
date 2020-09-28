8553f6ea411008b83d7a5a0d0ae6b151b4f8ff66
Kayan-dev <kaya-development@outlook.com>
2020-09-28 12:00:35 +0200

    fixing typo

--- models/story.js
+++ models/story.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      story.belongsTo(homepage),
+      story.belongsTo(homnepage),
         {
           through: "homepage",
           foreignKey: "homepageId",
